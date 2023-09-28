3861e2eb353e84f69c3a22a21296a0d81b91761d
Ross Simpson <thisisrosssimpson@gmail.com>
2023-09-28 16:26:31 +0100

    Heroku tweaks pt 2

--- server.js
+++ server.js
@@ -15,9 +15,12 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 console.log('isProduction:', isProduction);
 
-const client = isProduction ? 
-  redis.createClient(process.env.REDIS_URL) : // Use the Heroku Redis URL in production
-  redis.createClient({ host: '127.0.0.1', port: 6379 }); // Use local Redis in development
+const client = redis.createClient(process.env.REDIS_URL) // Use the Heroku Redis URL in production
+
+
+// const client = isProduction ? 
+//   redis.createClient(process.env.REDIS_URL) : // Use the Heroku Redis URL in production
+//   redis.createClient({ host: '127.0.0.1', port: 6379 }); // Use local Redis in development
 
 client.on('connect', () => {
   console.log('Redis client connected');
